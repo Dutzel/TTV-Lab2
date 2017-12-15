f3f8cb8091d6f9c994ea3ed1b8f642dc6f8687ed
abp491 <fabian.reiber@haw-hamburg.de>
2017-12-15 17:40:58 +0100

    remove old code fixes #1

--- src/app/BattlePlan.java
+++ src/app/BattlePlan.java
@@ -1,15 +1,12 @@
 package app;
 
 import java.math.BigInteger;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.SynchronousQueue;
 
 import app.CoAPConnectionLED;
 import app.Strategy;
-import de.uniba.wiai.lspi.chord.com.Node;
 import de.uniba.wiai.lspi.chord.data.ID;
 import de.uniba.wiai.lspi.chord.service.NotifyCallback;
 import de.uniba.wiai.lspi.chord.service.impl.ChordImpl;
@@ -26,12 +23,7 @@ public class BattlePlan implements NotifyCallback{
 
 	private ChordImpl impl;
 	private BigInteger maxNodekey;
-	//private List<Node> sortedFingerTable;
-	private boolean firstShotReceived;
-	//private List<BigInteger> shipPositions;
 	private Map<ShipInterval, Boolean> shipPositions;
-	//private List<ID> hittedEnemyShips;
-	//private Map<ID, Integer> enemiesWithShipCount;
 	private CoAPConnectionLED cCon;
 	
 	/**
@@ -46,19 +38,12 @@ public class BattlePlan implements NotifyCallback{
 
 	public BattlePlan(ChordImpl impl, String coapUri, Strategy strategy) {
 		this.impl = impl;
-		//shipPositions = new ArrayList<BigInteger>();
 		this.shipPositions = new HashMap<ShipInterval, Boolean>();
-		//hittedEnemyShips = new ArrayList<ID>();
-		//enemiesWithShipCount = new HashMap<ID, Integer>();
-		firstShotReceived = false;
-		//initEnemiesWithShipCount();
-		//setupShipPositioning(null);
 		calcMaxNodekey();
-		startStrategie(null);
 		
 		// init coap interface and set led status to green
-		
-		//this.cCon = new CoAPConnectionLED(coapUri);
+		// TODO: einkommentieren!!
+//		this.cCon = new CoAPConnectionLED(coapUri);
 //		this.cCon.turnOn();
 //		this.cCon.setColor("g");
 		this.strategy = strategy;
@@ -80,16 +65,10 @@ public class BattlePlan implements NotifyCallback{
 		// our turn: call retrieve on another node which we need to choose: this.chordImpl.retrieve(target);
 		//	set this.lastShotTarget = target; to recognize later in broadcast, if we made the shot
 		
-		
-		//boolean inRange = true; // If this function is called... does that directly mean, that we received shot in our range?
-		
 		//check if target is in one of our intervals where we placed a ship
 		boolean hit = checkShipPlacement(target);
 		if(hit){
-			if(!firstShotReceived){
-				//setupShipPositioning(target);
-				this.shipPositions = this.strategy.shipPlacementStrategy();
-			}
+			// TODO: logging :)
 			this.setSensorColor();
 		}
 		this.impl.broadcast(target, hit);
@@ -110,18 +89,13 @@ public class BattlePlan implements NotifyCallback{
 		//				false: - register target and hit
 		//		set this.lastShotTarget = null; because if we were shot, then we will shoot, so we know if the last broadcast was ours
 		if(hit){
-			//hittedEnemyShips.add(target);
+			// Once an enemy got hit, we need to update his information.
 			this.strategy.addHitTarget(source, target);
-			// Once an enemy got hit, we need to update his information
-			// Later we can use this information to choose our next target
-			// e.g. if we intend to hit the enemy that already sustained the most hits.
-			//Integer count = enemiesWithShipCount.get(source);
 			Integer count = this.strategy.getEnemyShipCount(source);
 			if(count == null){
 				count = 0;
 			}
 			count += 1;
-			//enemiesWithShipCount.put(source, count);
 			this.strategy.putEnemyShipCount(source, count);
 			// we shot this enemy
 			/**
@@ -132,20 +106,17 @@ public class BattlePlan implements NotifyCallback{
 			 * dass ein Schuss auf sie erfolgreich stattfand?
 			 * --> Das Senden eines Broadcasts muss parallel (asynchron) zum übrigen Programmablauf erfolgen!
 			 */
-			if(this.lastShotTarget.equals(target)){  
-					//if(this.enemiesWithShipCount.get(source) == 10){
+			if(this.lastShotTarget.equals(target)){
 					if(count == 10){
 						//we win the game, but what to do now?
 					}
 			}
 		}
 		else{
-			//also remember no hits
+			// Once an enemy got no hit, we need to update his information.
 			this.strategy.addNoHitTarget(source, target);
 		}
 		this.lastShotTarget = null;
-		
-		//startStrategie(target);
 	}
 
 	public void loadGrid(){
@@ -182,55 +153,8 @@ public class BattlePlan implements NotifyCallback{
 		 */
 		if(this.impl.getID().toBigInteger().compareTo(this.impl.getFingerTable().get(0).getNodeID().toBigInteger()) == -1){
 			System.err.println("I am (" + this.impl.getID() + ") the very first player allowed to shoot!");
+			this.shoot(this.chooseTarget()); // We are the first player allowed to shoot
 		}
-	}
-	/**
-	 * This method implements our logic we want to follow to give the ships in our space a partiular position.
-	 */
-//	private void setupShipPositioning(ID target){
-//		if(target.equals(null)){
-//			// Do nothing and wait for the first player to shoot at us.
-//		}else{
-//			/** In this case we received a first shoot to our interval.
-//			 * We can use this situation to avoid beeing hitted by the very first shot on us.
-//			 * In order to do so, we should read the target id and place our ships around the first impact
-//			 * in our intervall.
-//			 */
-//			// TODO: Strategie to give 10 ships a position.
-//			refreshSortedFingerTable();
-//			impl.getID().toBigInteger();
-//			firstShotReceived = true;
-//		}
-//
-//	}
-
-	/**
-	 * This method depicts some kind of a facade for our battle strategy and therefore
-	 * calls all other strategy parts in intendet sequence.
-	 * @param target
-	 */
-	private void startStrategie(ID target){
-//		if(!target.equals(null)){
-//			refreshSortedFingerTable();
-//			sortedFingerTable.sort(Comparator.comparing(Node::getNodeID));
-//			/** Keep in mind that there is an method called isInInterval(fromID, toID)
-//			 * e.g.: hittedEnemyShips.get(0).isInInterval(fromID, toID)
-//			 */
-////			for (ID id : hittedEnemyShips) {
-////				if(enemiesWithShipCount.get(id) == 10 ){
-////					//this enemy has lost all his ships
-////				}
-////			}
-//			//
-//			boolean hitReceived = false; // or true
-//			this.impl.broadcast(this.impl.getID(), hitReceived);
-//			//TODO: Implement our strategy after retrieving an attack.
-//		}else{
-			// This case can only happen once we connect to the chord network.
-			if(impl.getID().toBigInteger() == maxNodekey){
-				this.shoot(this.chooseTarget()); // We are the first player allowed to shoot
-			}
-		//}
 	}
 
 	/**
@@ -251,13 +175,8 @@ public class BattlePlan implements NotifyCallback{
 		//TODO: broadcast to target.
 		this.lastShotTarget = target;
 		this.impl.retrieve(target);
-		//broadcast(impl.getID(), target, false);
 	}
 
-//	private void refreshSortedFingerTable(){
-//		sortedFingerTable = this.impl.getFingerTable(); // Question: Does the FingerTable change within the match?
-//		sortedFingerTable.sort(Comparator.comparing(Node::getNodeID));
-//	}
 	/**
 	 * This method calculates the max value of a nodekey with respect of the definition of our task
 	 */
@@ -266,21 +185,6 @@ public class BattlePlan implements NotifyCallback{
 		maxNodekey = maxNodekey.pow(160);
 		maxNodekey = maxNodekey.subtract(new BigInteger("1"));
 	}
-
-	/**
-	 * This method is used to initialize the HashMap enemiesWithShipCount.
-	 */
-//	private void initEnemiesWithShipCount(){
-//		sortedFingerTable = this.impl.getFingerTable(); // Question: Does the FingerTable change within the match?
-//		sortedFingerTable.sort(Comparator.comparing(Node::getNodeID));
-//		for (Node node : sortedFingerTable) {
-//			enemiesWithShipCount.put(node.getNodeID(), 0);
-//		}
-//		/**
-//		 * If the FingerTable changes within the match, we need to consider this in the implementation of the
-//		 * enemiesWithShipCount HashMap.
-//		 */
-//	}
 	
 	/**
 	 * Check if the target is in one of our interval we placed a ship in. 
